3a1b9761fa63b026c8313bd473c279aefa68a40c
Zachary d'Assumpcao <zachary.dassumpcao@paygle.com>
2026-03-09 14:49:03 +1030

    vault backup: 2026-03-09 14:49:03

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,12 +6,14 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
-  }),
+  footer: Component.Footer(
+    // {
+    // links: {
+    //   GitHub: "https://github.com/jackyzha0/quartz",
+    //   "Discord Community": "https://discord.gg/cRFFHYye7t",
+    // },
+  // }
+),
 }
 
 // components for pages that display a single page (e.g. a single note)
