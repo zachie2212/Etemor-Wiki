c9ffe9ebabdf43ed0afc9e1f3a0783dcf1444694
Zachary d'Assumpcao <zachary.dassumpcao@paygle.com>
2026-03-12 12:36:33 +1030

    vault backup: 2026-03-12 12:36:33

--- quartz.config.ts
+++ quartz.config.ts
@@ -69,7 +69,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false, disableBrokenWikilinks: true }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "absolute" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
