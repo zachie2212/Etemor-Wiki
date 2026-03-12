9e350958384d60e04782592cc77101210fb022f2
Zachary d'Assumpcao <zachary.dassumpcao@paygle.com>
2026-03-12 12:28:57 +1030

    vault backup: 2026-03-12 12:28:57

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "zachie2212.github.io/Etemor-Wiki",
     ignorePatterns: ["private", "templates", ".obsidian", "PRIVATE/**"],
     defaultDateType: "modified",
     theme: {
